867762b359832c02f2c42f67fe451dba3d3519dc
Apple <apple@Apples-MacBook-Air.local>
2017-10-28 15:52:38 +0530

    Added UI view by storyboard.

---
+++ ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  My Calculator
+//
+//  Created by Apple on 28/10/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func theButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.lightGray
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
